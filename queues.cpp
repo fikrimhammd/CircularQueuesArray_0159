9e0fb82f8a0e0f8ff1b4a12c1a62f8889bc47bbe
fikrimhammd <fikri.kdi60@gmail.com>
2024-05-30 11:54:37 +0700

    case 2

--- queues.cpp
+++ queues.cpp
@@ -126,6 +126,31 @@ int main() {
 				q.insert();
 				break;
 			}
-			
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+
+	return 0;
+}
+
+
 
 			
