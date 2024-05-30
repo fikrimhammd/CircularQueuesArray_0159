92a8a7a222945cb3b13cd457352d4d92b7f5df17
fikrimhammd <fikri.kdi60@gmail.com>
2024-05-30 11:53:21 +0700

    switch

--- enc_temp_folder/dfd83d73c27ae7e4ddac7e9af8fd4bdb/queues.cpp
+++ enc_temp_folder/dfd83d73c27ae7e4ddac7e9af8fd4bdb/queues.cpp
@@ -121,4 +121,34 @@ int main() {
 			cout << endl;
 
 			switch (ch) {
-			
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+	}
+
+	return 0;
+}
+
+
