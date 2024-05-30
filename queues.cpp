16cda41c213b5eee35f395461318a0f3e3bc40df
fikrimhammd <fikri.kdi60@gmail.com>
2024-05-30 10:56:21 +0700

    void remove

--- queues.cpp
+++ queues.cpp
@@ -7,6 +7,7 @@ class Queues {
 	int queue_array[5];
 
 
+
 public:
 	Queues() {
 		FRONT = -1;
@@ -39,4 +40,31 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue undeflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT == FRONT + 1;
+		}
+
+
+
+
+	}
+};
 
